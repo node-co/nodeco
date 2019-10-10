f1edcaded87edd00b5ce8b5b6206e55da27b3821
camilo <camilomontoya2@gmail.com>
2019-10-10 15:36:31 -0500

    configuración de vistas

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ const methodOverride = require("express-method-override")("_method");
 const app = express();
 const compression = require("compression");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const port = 3000;
+//const mongoose = require("mongoose");
+const viewDir = `${__dirname}/views`;
+app.set("views", viewDir);
+app.set("view engine", "pug");
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride);
+
+app.get("/", async (req, res) => {
+  res.render("pages/home", { variable: "hola soy una variable" });
+});
+
+app.listen(port, () => {
+  console.log(`API REST corriendo en http://localhost:${port}`);
+});
